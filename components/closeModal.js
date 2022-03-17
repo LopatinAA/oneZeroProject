6ef901d1fdf06d65c7d857abbbeeb7e4024825f2
Anrey <lopatkin.andrushka@yandex.ru>
2022-03-18 00:24:57 +0300

    Задание по HTML

--- components/closeModal.js
+++
@@ -1,9 +0,0 @@
-
-
-const closeModal = () => {
-    modalResult.style.display = 'none';
-    document.querySelectorAll('div.box').forEach(function(el){
-        el.innerHTML = '';
-    });
-    move = 0;
-}
--- components/prepartReult.js
+++
@@ -1,7 +0,0 @@
-
-
-const prepartResult = (winner,scoreCross,scoreZero) => {
-    contentWrapper.innerHTML = `Победили ${winner} ! <br> Крестики ${scoreCross} - ${scoreZero} Нолики`;
-    modalResult.style.display = 'block'
-    arrNull = ['null','null','null','null','null','null','null','null','null',];
-}
